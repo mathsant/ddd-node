b5afc348339e6de194ea001e7a079795e70d6878
mathsant <mathsant_99@hotmail.com>
2025-05-07 19:45:49 -0300

    feat: adding order tests

---
+++ src/entity/order.spec.ts
@@ -0,0 +1,45 @@
+import { Order } from "./order";
+import { OrderItem } from "./order_item";
+
+describe("Order unit test", () => {
+  it("should throw error when id is empty", () => {
+    expect(() => {
+      new Order("", "123", []);
+    }).toThrow("Id is required.");
+  });
+
+  it("should throw error when customerId is empty", () => {
+    expect(() => {
+      new Order("1", "", []);
+    }).toThrow("CustomerID is required.");
+  });
+
+  it("should throw error when customerId is empty", () => {
+    expect(() => {
+      new Order("1", "123", []);
+    }).toThrow("Items are required.");
+  });
+
+  it("should calculate total", () => {
+    const item = new OrderItem("1", "Item 1", 10, "Product 1", 2);
+    const item2 = new OrderItem("2", "Item 2", 20, "Product 2", 2);
+
+    const order = new Order("Order1", "123", [item]);
+
+    let total = order.total();
+
+    expect(total).toBe(20);
+
+    const order2 = new Order("Order2", "123", [item, item2]);
+    total = order2.total();
+
+    expect(total).toBe(60);
+  });
+
+  it("should throw error if the qty is less or equal to zero", () => {
+    expect(() => {
+      const item = new OrderItem("1", "Item 1", 10, "Product 1", 0);
+      const order = new Order("Order1", "123", [item]);
+    }).toThrow("Quantity must be greater than 0.");
+  });
+});
